eb4aea9c6ce85f5e21e49c39836b33e40c4a9f96
nielroger <nielrogerekka94@gmail.com>
2021-11-11 11:44:00 +0530

    Create TestPython.py

---
+++ Folder/TestPython.py
@@ -0,0 +1,18 @@
+import sys
+import requests
+import json
+
+if __name__ == '__main__':
+    arg_dict=dict()
+
+    response = ""
+    try:
+      message = sys.argv[1]   
+                
+      
+    except Exception as e:
+      message = {"Error": "Error in running Script, Error=>" + str(e)}
+      message = json.dumps(message)
+      code=400
+
+    print(str(message))         

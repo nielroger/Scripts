86667b40c6b465e2e73c9f96bd5706461678d21c
nielroger <nielrogerekka94@gmail.com>
2021-12-23 17:14:52 +0530

    Create providers.tf

---
+++ TF/providers.tf
@@ -0,0 +1,127 @@
+provider "aws" {
+  alias      = "apeast1"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "ap-east-1"
+}
+provider "aws" {
+  alias      = "apnortheast1"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "ap-northeast-1"
+}
+provider "aws" {
+  alias      = "apnortheast2"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "ap-northeast-2"
+}
+provider "aws" {
+  alias      = "apnortheast3"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "ap-northeast-3"
+}
+provider "aws" {
+  alias      = "apsouth1"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "ap-south-1"
+}
+provider "aws" {
+  alias      = "apsoutheast1"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "ap-southeast-1"
+}
+provider "aws" {
+  alias      = "apsoutheast2"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "ap-southeast-2"
+}
+provider "aws" {
+  alias      = "cacentral1"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "ca-central-1"
+}
+provider "aws" {
+  alias      = "cnnorth1"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "cn-north-1"
+}
+provider "aws" {
+  alias      = "cnnorthwest1"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "cn-northwest-1"
+}
+provider "aws" {
+  alias      = "eucentral1"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "eu-central-1"
+}
+provider "aws" {
+  alias      = "eunorth1"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "eu-north-1"
+}
+provider "aws" {
+  alias      = "euwest1"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "eu-west-1"
+}
+provider "aws" {
+  alias      = "euwest2"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "eu-west-2"
+}
+provider "aws" {
+  alias      = "euwest3"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "eu-west-3"
+}
+provider "aws" {
+  alias      = "mesouth1"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "me-south-1"
+}
+provider "aws" {
+  alias      = "saeast1"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "sa-east-1"
+}
+provider "aws" {
+  alias      = "useast1"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "us-east-1"
+}
+provider "aws" {
+  alias      = "useast2"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "us-east-2"
+}
+provider "aws" {
+  alias      = "uswest1"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "us-west-1"
+}
+provider "aws" {
+  alias      = "uswest2"
+  secret_key = var.awssecretkey
+  access_key = var.awsaccesskey
+  region     = "us-west-2"
+}
+
